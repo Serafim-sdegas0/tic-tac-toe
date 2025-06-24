7ed5e167bba81e1daeac1e900b5319981adfd39d
Serafim-sdegas0 <Serafims.sergijenko@outlook.com>
2025-06-24 14:06:42 +0300

    Feature/react (#4)
    * src
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Hello world
    
    * build
    
    ---------
    
    Co-authored-by: Serafims-degas0 <Serafims-degas0>

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+          <><p>
+      Hello World.
+    </p><a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      </a></>
+  );
+}
+
+export default App;
