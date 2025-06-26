79dd66881ea71e8570de191d31d2cb66f252807b
Serafims-degas0 <Serafims-degas0>
2025-06-26 18:25:46 +0300

    add sqvers

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,29 @@ function App() {
   const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
-    <>
-      Hello World.
+    <div style={styles.board}>
       {
         numbers.map((num) => (
-          <>{num}</>
+          <button style={styles.square} key={num}>{num}</button>
         ))
       }
-    </>
+    </div>
   );
 }
-
+const styles = {
+  board: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 100px)',
+    gridGap: '5px',
+    marginTop: '50px',
+    justifyContent: 'center',
+  },
+  square: {
+    width: '100px',
+    height: '100px',
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+};
 export default App;
