1f6b5a72ec7b8169b379da2c2d70849c41a1e852
Serafim-sdegas0 <Serafims.sergijenko@outlook.com>
2025-06-28 14:19:45 +0300

    Feature/UI bottns (#5)
    * adding arry nambers
    
    * add sqvers
    
    * from no to yes
    
    ---------
    
    Co-authored-by: Serafims-degas0 <Serafims-degas0>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-          <><p>
-      Hello World.
-    </p><a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      </a></>
-  );
+type buttonState = {
+  num: Number,
+  isSelected: Boolean
 }
 
+function App() {
+
+  const [buttonStates, setButtonStates] = useState<buttonState[]>([
+    { num: 0, isSelected: false },
+    { num: 1, isSelected: false },
+    { num: 2, isSelected: false },
+    { num: 3, isSelected: false },
+    { num: 4, isSelected: false },
+    { num: 5, isSelected: false },
+    { num: 6, isSelected: false },
+    { num: 7, isSelected: false },
+    { num: 8, isSelected: false },
+  ]);
+
+  const handleClick = (num: Number) => {
+    const updated = buttonStates.map((bs) =>
+      bs.num === num ? { ...bs, isSelected: true } : bs
+    );
+    setButtonStates(updated);
+
+  }
+  return (
+    <div style={styles.board}>
+      {
+        buttonStates.map((bs) => (
+          <button style={styles.square} key={bs.num.toString()} onClick={() => handleClick(bs.num)} >{bs.isSelected ? "yes" : "no"}</button>
+        ))
+      }
+    </div>
+  );
+}
+const styles = {
+  board: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 100px)',
+    gridGap: '5px',
+    marginTop: '50px',
+    justifyContent: 'center',
+  },
+  square: {
+    width: '100px',
+    height: '100px',
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+};
 export default App;
