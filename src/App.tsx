78b6036b68c1b8dd2484a6e8ce0312e9541e25a5
Serafims-degas0 <Serafims-degas0>
2025-07-14 10:55:56 +0300

    Finish! I did it

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,33 @@ import './App.css';
 type buttonState = {
   num: Number;
   symbol: string;
-}
+};
+
+export const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
-
   const [buttonStates, setButtonStates] = useState<buttonState[]>
     (Array.from({ length: 9 }, (_, i) => ({ num: i, symbol: '' }))
     );
   const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>('X');
   const [count, setCount] = useState(0)
   const [xcount, setxCount] = useState(0)
+  const [winner, setwinner] = useState<'X' | 'O' | null>(null);
+
+
 
   const handleClick = (num: Number) => {
+    if (winner) return;
+
     const square = buttonStates.find(bs => bs.num === num);
     if (square?.symbol) return;
 
@@ -23,19 +38,19 @@ function App() {
       bs.num === num ? { ...bs, isSelected: true, symbol: currentPlayer } : bs
     );
 
-
-
     setButtonStates(updated);
 
-    const winner = checkWinner(updated);
-    if (winner) {
-      if (winner === 'X') setCount(prev => prev + 1);
-      else if (winner === 'O') setxCount(prev => prev + 1);
-      
+
+    const result = checkWinner(updated);
+    if (result) {
+      setwinner(result);
+      if (result === 'X') setCount(prev => prev + 1);
+      else if (result === 'O') setxCount(prev => prev + 1);
     } else {
       setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
     }
   };
+
   const checkWinner = (board: buttonState[]): 'X' | 'O' | null => {
     for (let combo of winCombinations) {
       const [a, b, c] = combo;
@@ -50,22 +65,30 @@ function App() {
     return null;
   };
 
+  const restartGame = () => {
+    setButtonStates(Array.from({ length: 9 }, (_, i) => ({ num: i, symbol: '' })));
+    setCurrentPlayer('X');
+    setwinner(null);
+  };
+
+
   return (
     <>
       <div style={{ textAlign: 'center' }}>
         <h2>Player X Wins: {count}</h2>
         <h2>Player O Wins: {xcount}</h2>
+        {winner && <h2>🎉 Player {winner} Wins!</h2>}
+        {winner && <button onClick={restartGame}>Restart Game</button>}
       </div>
       <div style={styles.board}>
-        {
-          buttonStates.map((bs) => (
+        {buttonStates.map((bs) => (
             <button style={styles.square} key={bs.num.toString()} onClick={() => handleClick(bs.num)} >{bs.symbol}</button>
-          ))
-        }
+          ))}
       </div>
     </>
   );
 }
+
 const styles = {
   board: {
     display: 'grid',
@@ -82,14 +105,5 @@ const styles = {
     cursor: 'pointer',
   },
 };
-export const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
+
 export default App;
